68a1961a3cdb7a302f2c10dc093b8464fe6dd663
root <root@srh-test1.cems.rl.ac.uk>
2019-03-19 16:22:24 +0000

    Updated to provide a csv with hourly download info from a standard dhus log

--- scripts/dhus_download_per_hour.sh
+++ scripts/dhus_download_per_hour.sh
@@ -16,22 +16,30 @@ month=`head -1 $log | awk '{print $1}' | sed 's/\]\[/\t/g' | awk '{print $2}' | 
 day=`head -1 $log | awk '{print $1}' | sed 's/\]\[/\t/g' | awk '{print $2}' | tr '-' '\t' | awk '{print $3}'`
 	
 total=0
+total_volume=0
 
 for i in {00..23};
 do
 
     #calculate volume for this hour
     hour_volume=0
-    for j in `grep "\[0\.13\.4\-22\]\[${year}\-${month}\-${day} ${i}\:" $log | grep stringtogrep /srh/data/logs/dhus.log | grep -o '(.*)' | awk '{print $1}'  | sed 's/[(,)]//g'`;
+    hour_volume_gb=0
+    for j in `grep "\[0\.13\.4\-22\]\[${year}\-${month}\-${day} ${i}\:" $log | grep "${stringtogrep}" | grep -o '(.*)' | awk '{print $1}'  | sed 's/[(,)]//g'`;
     do
-        hour_volume=`expr $j + $hour_volume
+        #hour_volume=`expr $j + $hour_volume`
+        hour_volume=`python -c "print ${j} + ${hour_volume}"`
+	hour_volume_gb=`python -c "print ${hour_volume} / 1024 / 1024 / 1024"`
     done
 
-	number=`grep "\[0\.13\.4\-22\]\[${year}\-${month}\-${day} ${i}\:" $log | grep -c "${stringtogrep}"`
+    number=`grep "\[0\.13\.4\-22\]\[${year}\-${month}\-${day} ${i}\:" $log | grep -c "${stringtogrep}"`
 
-	echo -e "Hour ${i}: ${number}\t Volume downloaded: ${hour_volume}"
+    #echo -e "Hour ${i}: ${number}\t Volume downloaded: ${hour_volume_gb}"
+    #echo -e "${day}/${month}/${year} ${i}:00:00, ${number},${hour_volume_gb}"
+    echo -e "${year}-${month}-${day}T${i}:00:00, ${number},${hour_volume_gb}"
 
-	total=`expr $number + $total`
+    total=`expr $number + $total`
+    total_volume=`python -c "print ${total_volume} + ${hour_volume_gb}"`
 done
 
-echo -e "\nTotal: ${total}"
+#echo -e "\nTotal files: ${total}"
+#echo -e "\nTotal download (Gb): ${total_volume}"
