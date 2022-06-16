7ac9c6555c54d627e5dfb4498f0097b0e86d35a8
root <root@srh-services7.ceda.ac.uk>
2022-06-16 09:54:35 +0100

    Updated so further info added on alignment report

--- management_scripts/scripts/bulk_alignment_checker.sh
+++ management_scripts/scripts/bulk_alignment_checker.sh
@@ -5,6 +5,7 @@
 # Secondary (srh with actual source hub)
 # Source-to-Primary (alignment of source hub with primary hub).
 # BE-FE (alignment of backend and frontend SRH hubs).
+
 if [ $# != 6 ]
 then
         echo "Usage: <days back to check i.e. 5> <product string i.e. S1A> <primary order config i.e. colhub> <secondary order config i.e. greek> <src-primary config> <BE-FE c
@@ -37,11 +38,19 @@ do
         cnt=1
         for j in "${primary}" "${secondary}" "${sourceTOprimary}" "${be_to_fe}" ;do
 
-                #get some summary info so can see which hubs are synchronising
-                which_hubs=`grep "target:" $j | awk '{print $2}' | tr '\n' ' ' | awk '{print $1" => "$2}'`
-                aligned=`python3 $script_loc -c $j -N -S $days_ago -E $days_ago -p $prod_string | grep aligned | awk '{print $4}'`
-                #aligned=`python3 $script_loc -c $j -N -S $days_ago -E \`date --date='1 day ago' +\%d-\%m-\%Y\` -p $prod_string | grep aligned | awk '{print $4}'`
+                #aligned=`python3 $script_loc -c $j -N -S $days_ago -E $days_ago -p $prod_string | grep aligned | awk '{print $4}'`
+                scriptop_ingest=`python3 $script_loc -c $j -N -S $days_ago -E $days_ago -p $prod_string -I`
+                scriptop_aqc=`python3 $script_loc -c $j -N -S $days_ago -E $days_ago -p $prod_string`
 
+                #need double quotes to preserve script op structure https://stackoverflow.com/questions/613572/capturing-multiple-line-output-into-a-bash-variable
+                firsthubnum_ingest=`echo "$scriptop_ingest" | grep Identified | head -1 | awk '{print $2}'`
+                secondhubnum_ingest=`echo "$scriptop_ingest" | grep Identified | head -2 | tail -1 | awk '{print $2}'`
+                aligned_ingest=`echo "$scriptop_ingest" | grep aligned | awk '{print $4}'`
+
+                firsthubnum_aqc=`echo "$scriptop_aqc" | grep Identified | head -1 | awk '{print $2}'`
+                secondhubnum_aqc=`echo "$scriptop_aqc" | grep Identified | head -2 | tail -1 | awk '{print $2}'`
+                aligned_aqc=`echo "$scriptop_aqc" | grep aligned | awk '{print $4}'`
+                
                 #should always follow this sequence if stick to this convention in the config files.
                 if [ $cnt -eq 1 ]
                 then
@@ -57,11 +66,26 @@ do
                         msg="BE-FE"
                 fi
 
-                echo "${msg} (${which_hubs}) product: ${prod_string}  ${aligned}%"
+                #get some summary info so can see which hubs are synchronising
+                #which_hubs=`grep "target:" $j | awk '{print $2}' | tr '\n' ' ' | awk '{print $1" => "$2}'`
+                ref_hub=`grep "target:" $j | awk '{print $2}' | tr '\n' ' ' | awk '{print $1}'`
+                target_hub=`grep "target:" $j | awk '{print $2}' | tr '\n' ' ' | awk '{print $2}'`
+
+                #echo "${msg} IngestDate [${which_hubs} ${firsthubnum_ingest}/${secondhubnum_ingest}] product: ${prod_string}  ${aligned_ingest}%"
+                #echo "${msg} AqcuisitionDate [${which_hubs} ${firsthubnum_aqc}/${secondhubnum_aqc}] product: ${prod_string}  ${aligned_aqc}%"
+
+                echo "${msg} IngestDate [${ref_hub} (${firsthubnum_ingest})  => ${target_hub} (${secondhubnum_ingest})] product: ${prod_string}  ${aligned_ingest}%"
+                echo "${msg} AqcuisitionDate [${ref_hub} (${firsthubnum_aqc})  => ${target_hub} (${secondhubnum_aqc})] product: ${prod_string}  ${aligned_aqc}%"
+                echo " "
 
                 cnt=`expr $cnt + 1`
 
-                aligned=""
+                firsthubnum_ingest=""
+                secondhubnum_ingest=""
+                firsthubnum_aqc=""
+                secondhubnum_aqc=""
+                aligned_ingest=""
+                aligned_aqc=""
         done
         echo -e "\n"
 done
