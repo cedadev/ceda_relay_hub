2d9c0181160936c3080d3fd901f5d5410606f54a
Steve Donegan <SteveDonegan@users.noreply.github.com>
2024-11-13 16:20:33 +0000

    Update extract_pub_delay_strings.sh
    developed on local /srh_data_incoming_6/dhus_monitor_software/ceda_relay_hub on srh18 but git issues.  So just copied updates via git gui...

--- management_scripts/scripts/extract_pub_delay_strings.sh
+++ management_scripts/scripts/extract_pub_delay_strings.sh
@@ -10,35 +10,35 @@
 scan_directory () {
 
         dirstr=$1
+        data=""
 
-        if [ -d $dirstr ]
+        if [ -d "$dirstr" ]
         then
 
                 #is there a file report for that day (not always one if hub issues
-                results=`find $dirstr -name "${datestr}*.txt" -print | wc -l`
+                results=$(find "$dirstr" -name "${datestr}*.txt" -print | wc -l)
 
-                if [ $results != 0 ]
+                if [ "$results" != 0 ]
                 then
                         #echo "result!"
-                        found_file=`find $dirstr -name "${datestr}*.txt" -print | head -1`
-                        #echo $found_file
-                        data=`cat $found_file | grep 'Source:' $found_file | grep  $hub_str | awk '{print $(NF-1)}'`
-                        datalen=`expr "$data" : '.*'`
+                        found_file=$(find "$dirstr" -name "${datestr}*.txt" -print | head -1)
+                        #data=$(cat "$found_file" | grep 'Source:' "$found_file" | grep  "$hub_str" | awk '{print $(NF-1)}')
+                        data=$(grep 'Source:' "$found_file" | grep  "$hub_str" | awk '{print $(NF-1)}')
+                        datalen=$(expr "$data" : '.*')
 
-                        if [ $datalen != 0 ] && [ $data != "source" ]
+                        if [ "$datalen" != 0 ] && [ "$data" != "source" ]
                         then
-                                echo $data
-
+                                echo "$data"
 
                         else
-                                echo ""
+                                echo "$data"
                         fi
                 else
-                        echo ""
+                        echo "$data"
                 fi
 
         else
-                echo ""
+                echo "$data"
 
         fi
 
@@ -50,19 +50,17 @@ then
         exit
 fi
 
-backup_report_dir="/srh_data_incoming_4/srh_logs/${HOSTNAME}/publication_delay"
+backup_report_dir="/srh_data_incoming_5/srh_logs/${HOSTNAME}/publication_delay"
 local_report_dir="/var/www/html/srh_monitoring/publication_delay/"
 
 month=$1
 year=$2
 hub_str=$3
 
-tmpfile=".report.tmp"
-
-if [ $month -eq 01 ] || [ $month -eq 03 ] || [ $month -eq 05 ] || [ $month -eq 07 ] || [ $month -eq 08 ] || [ $month -eq 10 ] || [ $month -eq 12 ]
+if [ "$month" -eq 01 ] || [ "$month" -eq 03 ] || [ "$month" -eq 05 ] || [ "$month" -eq 07 ] || [ "$month" -eq 08 ] || [ "$month" -eq 10 ] || [ "$month" -eq 12 ]
 then
         days=31
-elif [ $month -eq 02 ]
+elif [ "$month" -eq 02 ]
 then
         days=28 # fudge not expecting a leap year during this project...
 else
@@ -75,33 +73,41 @@ for day in $( eval echo {01..$days} )
 do
         datestr="${day}-${month}-${year}"
 
-        weeknum=`date -d "${year}-${month}-${day}" +%W`
+        weeknum=$(date -d "${year}-${month}-${day}" +%W)
 
         dirstr="${backup_report_dir}/${year}/$weeknum"
 
         #Scan archived delay reports first
-        retval=$( scan_directory $dirstr )
+        retval=$( scan_directory "$dirstr" )
 
-        retlen=`expr "$retval" : '.*'`
+        retlen=$(expr "$retval" : '.*')
 
         #note if successful string should be 17 - but some weirdness might occur so this works better than testing for 0
-        if [ $retlen -ge "5" ]
+        if [ "$retlen" -ge "5" ]
         then
-                echo "${year}-${month}-${day},${retval}"
+                retvalloc_abrv=$(echo "${retvalloc}" | tr '\n' ' ' | awk '{print $1}')
 
-                times="${times} ${retval}"
+                echo "${year}-${month}-${day},${retvalloc_abrv}"  #gets around weird double retval in the echo...
+                #printf "${year}-${month}-${day},${retval}\n"
+
+                times="${times} ${retvalloc_abrv}"
         else
 
                 #now try the main dir for younger reports
                 retvalloc=$( scan_directory $local_report_dir )
 
-                retloclen=`expr "$retvalloc=" : '.*'`
+                #retloclen=$(expr "$retvalloc=" : '.*')
+                retloclen=${#retvalloc}
 
-                if [ $retloclen -gt "5" ]
+                if [ "$retloclen" -ge "5" ]
                 then
-                        #echo $retvalloc
-                        echo "${year}-${month}-${day},${retvalloc}"
-                        times="${times} ${retvalloc}"
+                        retvalloc_abrv=$(echo "${retvalloc}" | tr '\n' ' ' | awk '{print $1}')
+                        
+                        #echo "${year}-${month}-${day},${retvalloc}" | tr '\n' ' ' | awk '{print $1}' #gets around weird double retval in the echo...
+                        echo "${year}-${month}-${day},${retvalloc_abrv}"
+                        #printf "${year}-${month}-${day},${retvalloc}"
+
+                        times="${times} ${retvalloc_abrv}"
 
                 else
                         echo "${year}-${month}-${day},NULL"
@@ -115,11 +121,12 @@ done
 totsecs=0
 for i in $times
 do
-        secs=`echo $i | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'`
-        totsecs=`expr $totsecs + $secs`
+        secs=$(echo $i | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+        #echo $secs
+        totsecs=$(expr $totsecs + $secs)
 done
-
-avgsecs=`expr $totsecs / $days`
-average_delay=`date -ud "@$avgsecs" +' %H:%M:%S'`
+#echo $totsecs
+avgsecs=$(expr "$totsecs" / "$days")
+average_delay=$(date -ud "@$avgsecs" +' %H:%M:%S')
 
 echo "Average delay: ${average_delay}"
